3a6a0c190d1b967257093a43d246d0a9315ca19b
Sander-Brilman <brilmansander@gmail.com>
2023-10-16 20:33:25 +0200

    added project

---
+++ PersonalNotes/Client/Services/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonalNotes.Client.Services;
+
+public class UserRepository
+{
+    private readonly HttpClient _httpClient;
+
+    public UserRepository(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task 
+
+    public async Task CreateUser(string email, string password)
+    {
+
+    }
+
+    public async Task<bool> LogUserIn(string email, string password)
+    {
+
+
+    }
+}
