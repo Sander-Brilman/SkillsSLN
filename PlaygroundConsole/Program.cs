bd404400bb76383d3fec0caf26a6e8b292831521
sander brilman <brilmansander@gmail.com>
2023-10-18 19:19:37 +0200

    sss

---
+++ PlaygroundConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+
+Claim emailClaim = new(ClaimTypes.Email, "brilmansander@gmail.com");
+Claim usernameClaim = new(ClaimTypes.Name, "sander");
+
+Claim[] claims = new[] { emailClaim, usernameClaim };
+
+ClaimsIdentity identity = new ClaimsIdentity(claims, "qwdqw");
+
+
+
+
+Console.WriteLine(identity.IsAuthenticated);
+
+Console.ReadLine();
